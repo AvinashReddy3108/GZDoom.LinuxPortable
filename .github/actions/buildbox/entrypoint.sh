7ad29bc8e012ae7e423e55b6175135a4874fb3fd
Avinash Reddy <avinashreddy3108+github@gmail.com>
2024-06-29 19:33:26 +0530

    workflows: packaging is now handled by container

--- .github/actions/buildbox/entrypoint.sh
+++ .github/actions/buildbox/entrypoint.sh
@@ -7,9 +7,7 @@ mkdir -pv parts/zmusic/source_code/build
 
 # Build
 cd parts/zmusic/source_code/build
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr && make install
-
-# ========================
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr && make -j$(nproc --all) install
 
 # Move to workspace directory
 cd "${OLDPWD}"
@@ -21,9 +19,23 @@ mkdir -pv gzdoom/source_code/build
 
 # Build
 cd gzdoom/source_code/build
-cmake .. -DNO_GTK=ON -DCMAKE_BUILD_TYPE=Release && make
-
-# ========================
+cmake .. -DNO_GTK=ON -DCMAKE_BUILD_TYPE=Release && make -j$(nproc --all)
 
 # Move to workspace directory
 cd "${OLDPWD}"
+
+# ===== Make package =====
+
+mkdir -pv out
+
+cp -v gzdoom/source_code/build/gzdoom out/
+cp -v gzdoom/source_code/build/*.pk3 out/
+
+ldd out/gzdoom | awk 'NF == 4 { system("cp -v " $3 " out/") }'
+cp -v /lib/x86_64-linux-gnu/libopenal.so.1 out/
+
+cp -Rv gzdoom/source_code/build/soundfonts/ out/soundfonts
+cp -Rv gzdoom/source_code/build/fm_banks/ out/fm_banks
+
+# [HACK] Force success, we'll handle this later
+exit 0
